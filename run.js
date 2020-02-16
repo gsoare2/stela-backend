62d2b344799b55ef9628851c55590f92718abf78
Gustavo <gustavo.silva6785@gmail.com>
2020-02-16 15:36:15 -0300

    add files

--- run.js
+++ run.js
@@ -1,9 +1,8 @@
-const express = require("express");
+import express from "express";
 const app = express();
-const port = 8080;
 
-app.get("/", (req, res) => {
-	res.send("Hello world!");
+app.get("/", function (req, res) {
+  res.send("Hello World")
 });
 
-app.listen(port, () => console.log(`server started at http://localhost:${port}`));
+app.listen(3000);
